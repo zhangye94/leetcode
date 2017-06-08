736d4d7a96466b577edc69ee90cb1ab14996372e
Zhang Ye <727935886@qq.com>
2017-06-08 14:44:32 +0800

    第412题

---
+++ src/Easy/P412FizzBuzz.java
@@ -0,0 +1,33 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P412FizzBuzz {
+	public static List<String> fizzBuzz(int n) {
+        List<String> ret = new ArrayList<String>(n);
+        for(int i=1,fizz=0,buzz=0;i<=n ;i++){
+            fizz++;
+            buzz++;
+            if(fizz==3 && buzz==5){
+                ret.add("FizzBuzz");
+                fizz=0;
+                buzz=0;
+            }else if(fizz==3){
+                ret.add("Fizz");
+                fizz=0;
+            }else if(buzz==5){
+                ret.add("Buzz");
+                buzz=0;
+            }else{
+                ret.add(String.valueOf(i));
+            }
+        } 
+        return ret;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		fizzBuzz(15);
+	}
+
+}

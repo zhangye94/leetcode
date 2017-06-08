ebed644405966f2e238f0d43b6f50e3021834767
Zhang Ye <727935886@qq.com>
2017-06-08 18:55:28 +0800

    第9题

---
+++ src/Easy/P9PalindromeNumber.java
@@ -0,0 +1,18 @@
+package Easy;
+
+public class P9PalindromeNumber {
+    public static boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+        	rev = rev*10 + x%10;
+        	x = x/10;
+        }
+        return (x==rev || x==rev/10);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isPalindrome(1121));
+	}
+
+}

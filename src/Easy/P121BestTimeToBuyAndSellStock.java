99de8a2c0d4ae79530e7d32326be02febc32002c
Zhang Ye <727935886@qq.com>
2017-06-13 22:05:50 +0800

    第260题

---
+++ src/Easy/P121BestTimeToBuyAndSellStock.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class P121BestTimeToBuyAndSellStock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Easy/P260SingleNumberIII.java
+++ src/Easy/P260SingleNumberIII.java
@@ -2,13 +2,30 @@ package Easy;
 
 public class P260SingleNumberIII {
 	public static int[] singleNumber(int[] nums) {
-        
+        int diff = 0;
+        for (int num : nums) {
+            diff ^= num;
+        }
+        diff &= -diff;
+        int[] rets = {0, 0};
+        for (int num : nums)
+        {
+            if ((num & diff) == 0)
+            {
+                rets[0] ^= num;
+            }
+            else
+            {
+                rets[1] ^= num;
+            }
+        }
+        return rets;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = {1, 2, 1, 3, 2, 5};
 		int[] res = singleNumber(arr);
-		for(int i=0;i<arr.length;i++){
+		for(int i=0;i<2;i++){
 			System.out.println(res[i]);
 		}
 	}

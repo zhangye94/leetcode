c90e8b21c919287004deb55b704b92f2bd1c534e
Zhang Ye <727935886@qq.com>
2017-06-13 22:25:00 +0800

    第121题

--- src/Easy/P121BestTimeToBuyAndSellStock.java
+++ src/Easy/P121BestTimeToBuyAndSellStock.java
@@ -1,10 +1,24 @@
 package Easy;
 
 public class P121BestTimeToBuyAndSellStock {
-
+	public static int maxProfit(int[] prices) {
+		if(prices.length == 0) return 0;
+		int low = prices[0];
+		int res = 0;
+		for(int i=0;i<prices.length;i++){
+			if(prices[i]<low){
+				low = prices[i];
+			}
+			if(res < prices[i] - low){
+				res = prices[i] - low;
+			}
+		}
+		return res;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] arr = {7, 6, 4, 3, 1};
+		System.out.println(maxProfit(arr));
 	}
 
 }
